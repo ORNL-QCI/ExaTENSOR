f89fef20be70a588a579b9040758a784e53ac582
Dmitry I. Lyakh <quant4me@gmail.com>
2014-07-07 17:12:03 -0400

    Errors corrected.

--- c_process.F90
+++ c_process.F90
@@ -1236,7 +1236,7 @@
             if(j1.eq.cuda_task_completed) then
              do j1=0,max_tensor_operands-1 !mark all tensor arguments as present on the GPU because the ETI has completed
               if(associated(my_eti%tens_op(j1)%op_aar_entry)) then
-               if(c_associated(my_eti%tens_op(j1)%op_aar_entry%tens_blck_c) then
+               if(c_associated(my_eti%tens_op(j1)%op_aar_entry%tens_blck_c)) then
                 j2=tensBlck_set_presence(my_eti%tens_op(j1)%op_aar_entry%tens_blck_c) !mark tensBlck_t as present on the GPU
                 if(j2.ne.0) write(jo_cp,'("ERROR(c_process::c_proc_life:nvcu_task_status): tensBlck_set_presence failed: ",i10)') j2
                endif
