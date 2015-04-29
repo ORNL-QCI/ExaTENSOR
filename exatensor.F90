ea3e281e76a8fc39ec8b638d21058c905427a953
Dmitry I. Lyakh <quant4me@gmail.com>
2015-04-29 16:34:30 -0400

    Moved some data from qforce.mod to exatensor.mod.

--- exatensor.F90
+++ exatensor.F90
@@ -14,6 +14,18 @@
         integer, private:: CONS_OUT=6     !default output for this module
         logical, private:: VERBOSE=.true. !verbosity for errors
         logical, private:: DEBUG=.true.   !debugging mode
+ !Numeric:
+        real(4), parameter, public:: eps4=epsilon(1.0) !single precision epsilon
+        real(8), parameter, public:: eps8=epsilon(1d0) !double precision epsilon
+        real(8), parameter, public:: zero_thresh=1d-11 !numerical comparison threshold: should account for possible round-off errors
+ !Kinds of MPI processes (process roles):
+        integer(INTD), parameter, public:: global_root=1             !global root
+        integer(INTD), parameter, public:: local_root=2              !local root
+        integer(INTD), parameter, public:: c_process_private=3       !computing process private to a unit cell
+        integer(INTD), parameter, public:: c_process_shared=4        !computing process shared by multiple unit cells
+        integer(INTD), parameter, public:: d_process=5               !I/O operating process
+        integer(INTD), parameter, public:: a_process=6               !accelerating process
+        integer(INTD), parameter, public:: c_procs_per_local_root=32 !number of C-processes per local root
 !TYPES:
 
 !DATA:
--- qforce.F90
+++ qforce.F90
@@ -1,28 +1,21 @@
-       module qforce !PROGRAM SPECIFIC MODULE
+!Quantum Chemistry specific module:
+!AUTHOR: Dmitry I. Lyakh (Liakh): quant4me@gmail.com
+!REVISION: 2015/04/28
+       module qforce
         use exatensor
         implicit none
 !PARAMETERS:
- !Numeric:
-        real(4), parameter, public:: eps4=epsilon(1.0) !single precision epsilon
-        real(8), parameter, public:: eps8=epsilon(1d0) !double precision epsilon
-        real(8), parameter, public:: zero_thresh=1d-11 !numerical comparison threshold: should account for possible round-off errors
- !Kinds of MPI processes (process roles):
-        integer, parameter, public:: global_root=1             !global root
-        integer, parameter, public:: local_root=2              !local root
-        integer, parameter, public:: c_process_private=3       !computing process private to a unit cell
-        integer, parameter, public:: c_process_shared=4        !computing process shared by multiple unit cells
-        integer, parameter, public:: d_process=5               !I/O operating process
-        integer, parameter, public:: a_process=6               !accelerating process
-        integer, parameter, public:: c_procs_per_local_root=32 !number of C-processes per local root
+
 !TYPES:
  !Nuclei vibration descriptor:
         type vibration_t
          real(8):: frequency
-         integer:: symmetry
+         integer(INTD):: symmetry
         end type vibration_t
  !Electronic state descriptor:
         type el_state_t
-         integer:: nelectrons                  !number of electrons in this state
+         integer(INTL):: nelectrons            !number of electrons in this state
+         integer(INTL):: degrees               !number of degrees of freedom
          real(8):: net_charge                  !net charge of the molecule in this state
          real(8):: spin                        !spin: <S^2>=spin*(spin+1)
          real(8):: sz                          !Z-projection of the spin
@@ -34,17 +27,17 @@
         end type el_state_t
  !Molecular descriptor:
         type molecule_t
-         integer:: natoms                             !number of atoms in the molecular system
-         integer:: number_of_states                   !number of electronic states computed (Hamiltonian roots)
+         integer(INTL):: natoms                       !number of atoms in the molecular system
+         integer(INTD):: nstates                      !number of electronic states computed (Hamiltonian roots)
          type(el_state_t), allocatable:: el_states(:) !description of the electronic states
-         integer:: basis_set_type=0                   !0 - Gaussian AO basis set; [1..] - alternative basis sets
+         integer(INTD):: basis_set_type=0             !0 - Gaussian AO basis set; [1..] - alternative basis sets
          real(8), allocatable:: atoms(:,:)            !0 - atomic number, 1..3 - X,Y,Z coordinates (a.u.) for each atom [1..natoms]
-         integer, allocatable:: basis(:)              !basis set ID for each atom
-         integer, allocatable:: tags(:)               !atomic tags
-         character(128):: mol_data_filename           !name of the file containing the rest of the molecular data
+         integer(INTD), allocatable:: basis(:)        !basis set ID for each atom
+         integer(INTD), allocatable:: tags(:)         !atomic tags
+         character(1024):: mol_data_filename          !name of the file containing the rest of the molecular data
         end type molecule_t
 !DATA:
 
-!INTERFACES:
+!FUNCTION VISIBILITY:
 
        end module qforce
