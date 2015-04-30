eee6db29207688fe25a8456ba0cad5c00df12e41
Dmitry I. Lyakh <quant4me@gmail.com>
2015-04-30 16:23:27 -0400

    Timers made threadsafe. Distributed methods modified.

--- distributed.F90
+++ distributed.F90
@@ -1,11 +1,12 @@
 !Distributed data storage primitives.
 !AUTHOR: Dmitry I. Lyakh (Liakh): quant4me@gmail.com
-!REVISION: 2015/04/27 (started 2015/03/18)
+!REVISION: 2015/04/30 (started 2015/03/18)
 !All rights reserved!
         module distributed
 !       use, intrinsic:: ISO_C_BINDING
-        use service_mpi !includes ISO_C_BINDING
+        use service_mpi !includes ISO_C_BINDING & MPI (must stay public)
         implicit none
+        public !because of mpi.mod (mpif.h)
 !PARAMETERS:
  !Output:
         integer, private:: CONS_OUT=6     !default output for this module
@@ -13,66 +14,105 @@
         logical, private:: DEBUG=.true.   !debugging mode
 !TYPES:
  !Global data location descriptor:
-        type, public:: DataLoc_t
-         integer(INT_MPI), private:: PrRank=-1 !MPI rank on which the data resides
-         integer(INT_MPI), private:: CommMPI   !MPI communicator the MPI rank refers to
-         integer(INT_MPI), private:: Window    !MPI window the data is exposed with
-         integer(INT_ADDR), private:: Offset   !Offset in the MPI window (in displacement units)
-         integer(INT_ADDR), private:: Volume   !Number of elements (each element byte size = displacement unit)
-         integer(INT_MPI), private:: DispUnit  !Displacement unit size in bytes
+        type, public:: DataDescr_t
+         integer(INT_MPI), private:: PrRank=-1  !MPI rank on which the data resides
+         integer(INT_MPI), private:: Window     !MPI window the data is exposed with
+         integer(INT_MPI), private:: CommMPI    !MPI communicator the MPI rank refers to
+         integer(INT_MPI), private:: DispUnit   !Displacement unit size in bytes
+         integer(INT_ADDR), private:: Offset    !Offset in the MPI window (in displacement units)
+         integer(INT_ADDR), private:: Volume    !Number of elements (each element size in bytes = displacement unit)
          contains
-          procedure, public:: init=>DataLocInit
-          procedure, public:: clean=>DataLocClean
-        end type DataLoc_t
+          procedure, public:: clean=>DataDescrClean      !clean a data descriptor
+          procedure, public:: init=>DataDescrInit        !set up a data descriptor
+!          procedure, public:: get_data=>DataDescrGetData !load data referred to by a data descriptor into a local buffer
+!          procedure, public:: put_data=>DataDescrPutData !store data from a local buffer in the location specified by a data descriptor
+        end type DataDescr_t
 !DATA:
 
 !FUNCTION VISIBILITY:
-        private DataLocInit
-        private DataLocClean
+        private DataDescrClean
+        private DataDescrInit
 
         contains
 !METHODS:
-!------------------------------------------------------------------------------
-        subroutine DataLocInit(this,process_rank,mpi_window,offset,volume,ierr)
-!DataLoc constructor.
+!-------------------------------------------
+        subroutine DataDescrClean(this,ierr)
+!Data descriptor cleaner.
         implicit none
-        class(DataLoc_t), intent(out):: this           !out: DataLoc object
-        integer(INT_MPI), intent(in):: process_rank    !in: process rank where the data resides
-        integer(INT_MPI), intent(in):: mpi_window      !in: MPI window the data is exposed with
-        integer(INT_ADDR), intent(in):: offset         !in: Offset in the MPI window (in displacement units)
-        integer(INT_ADDR), intent(in):: volume         !in: Number of elements (each element byte size = displacement unit)
-        integer(C_INT), intent(inout), optional:: ierr !out: error code (0:success)
-        integer(C_INT):: errc
-
-        errc=0
-        if(process_rank.ge.0) then
-         this%PrRank=process_rank
-         this%CommMPI=MPI_COMM_WORLD
-         this%Window=mpi_window
-         if(offset.ge.0.and.volume.ge.0) then
-          this%Offset=offset
-          this%Volume=volume
-         else
-          errc=1
-         endif
-        else
-         errc=2
-        endif
-        if(present(ierr)) ierr=errc
-        return
-        end subroutine DataLocInit
-!-----------------------------------------
-        subroutine DataLocClean(this,ierr)
-!DataLoc cleaner.
-        implicit none
-        class(DataLoc_t), intent(out):: this           !out: DataLoc object
-        integer(C_INT), intent(inout), optional:: ierr !out: error code (0:success)
-        integer(C_INT):: errc
+        class(DataDescr_t), intent(out):: this        !out: empty data descriptor
+        integer(INTD), intent(inout), optional:: ierr !out: error code (0:success)
+        integer(INTD):: errc
 
         errc=0
         this%PrRank=-1
         if(present(ierr)) ierr=errc
         return
-        end subroutine DataLocClean
+        end subroutine DataDescrClean
+!--------------------------------------------------------------------------------------------------------
+        subroutine DataDescrInit(this,process_rank,mpi_window,offset,volume,ierr,comm_mpi,check_thorough)
+!Data descriptor constructor.
+        implicit none
+        class(DataDescr_t), intent(inout):: this             !inout: data descriptor
+        integer(INT_MPI), intent(in):: process_rank          !in: process rank where the data resides
+        integer(INT_MPI), intent(in):: mpi_window            !in: MPI window the data is exposed with
+        integer(INT_ADDR), intent(in):: offset               !in: Offset in the MPI window (in displacement units)
+        integer(INT_ADDR), intent(in):: volume               !in: Number of elements (each element byte size = displacement unit)
+        integer(INTD), intent(inout), optional:: ierr        !out: error code (0:success)
+        integer(INT_MPI), intent(in), optional:: comm_mpi    !in: MPI communicator (default: MPI_COMM_WORLD)
+        logical(INTD), intent(in), optional:: check_thorough !in: if .true., enables the process order check (expensive): default=.false.
+        integer(INT_MPI):: comm,my_group,win_group,res,errc
+        integer(INT_ADDR):: attr
+        logical(INTD):: flag
+
+        errc=0
+        if(process_rank.ge.0) then
+         if(mpi_window.ne.MPI_WIN_NULL) then
+          this%PrRank=process_rank
+          this%Window=mpi_window
+          if(present(comm_mpi)) then; comm=comm_mpi; else; comm=MPI_COMM_WORLD; endif
+          call MPI_COMM_GROUP(comm,my_group,errc)
+          if(errc.eq.0) then
+           call MPI_WIN_GET_ATTR(mpi_window,MPI_WIN_DISP_UNIT,attr,flag,errc)
+           if(errc.eq.0.and.flag) then
+            this%DispUnit=int(attr,INT_MPI)
+            call MPI_WIN_GET_GROUP(mpi_window,win_group,errc)
+            if(errc.eq.0) then
+             if(present(check_thorough)) then; flag=check_thorough; else; flag=.false.; endif
+             res=MPI_UNEQUAL
+             if(flag) then
+              call MPI_WIN_GROUP_COMPARE(my_group,win_group,res,errc)
+             else
+              if(my_group.eq.win_group) res=MPI_IDENT
+             endif
+             if(errc.eq.0.and.res.eq.MPI_IDENT) then
+              if(offset.ge.0.and.volume.ge.0) then
+               this%Offset=offset
+               this%Volume=volume
+              else
+               errc=1
+              endif
+             else
+              errc=2
+             endif
+            else
+             errc=3
+            endif
+           else
+            errc=4
+           endif
+          else
+           errc=5
+          endif
+         else
+          errc=6
+         endif
+        else
+         errc=7
+        endif
+        if(errc.ne.0) this%clean()
+        if(present(ierr)) ierr=errc
+        return
+        end subroutine DataDescrInit
+!-------------------------------------------------------------------
 
         end module distributed
--- timers.F90
+++ timers.F90
@@ -1,7 +1,7 @@
        module timers
-!Timing services.
+!Timing services (threadsafe).
 !AUTHOR: Dmitry I. Lyakh (Liakh): quant4me@gmail.com
-!REVISION: 2015/04/27
+!REVISION: 2015/04/30
 !PUBLIC FUNCTIONS:
 ! # integer timer_start(real8:time_set, integer:time_handle);
 ! # logical time_is_off(integer:time_handle, integer:ierr[, logical:destroy]);
@@ -25,10 +25,11 @@
 #endif
 #endif
 !PARAMETERS:
-        integer, parameter, private:: max_timers=8192
-        integer, parameter, public:: timers_err_invalid_arg=1
-        integer, parameter, public:: timers_err_no_timers_left=2
-        integer, parameter, public:: timers_err_timer_null=3
+        integer, parameter, private:: MAX_TIMERS=8192
+        integer, parameter, public:: TIMERS_SUCCESS=0
+        integer, parameter, public:: TIMERS_ERR_INVALID_ARG=1
+        integer, parameter, public:: TIMERS_ERR_NO_TIMERS_LEFT=2
+        integer, parameter, public:: TIMERS_ERR_TIMER_NULL=3
 !TYPES:
         type, private:: timer_t
          real(8), private:: beg_time      !time the timer started (sec)
@@ -36,10 +37,10 @@
         end type timer_t
 !DATA:
         integer, private:: j_
-        type(timer_t), private:: timer(0:max_timers-1)=(/(timer_t(-1d0,-1d0),j_=0,max_timers-1)/)
-        integer, private:: handle_stack(0:max_timers-1)=(/(j_,j_=0,max_timers-1)/)
+        type(timer_t), private:: timer(0:MAX_TIMERS-1)=(/(timer_t(-1d0,-1d0),j_=0,MAX_TIMERS-1)/)
+        integer, private:: handle_stack(0:MAX_TIMERS-1)=(/(j_,j_=0,MAX_TIMERS-1)/)
         integer, private:: handle_sp=0
-        real(8), private:: timer_tick=-1d0 !must be negative
+        real(8), private:: timer_tick=-1d0 !uninitilized
 !FUNCTION VISIBILITY:
         public timer_start
         public time_is_off
@@ -52,38 +53,42 @@
         integer function timer_start(time_set,time_handle)
 !This function sets up a timer limited to <time_set> seconds and returns its handle in <time_handle>.
         implicit none
-        real(8), intent(in):: time_set     !requested time in microseconds
-        integer, intent(out):: time_handle !timer handle
+        real(8), intent(in):: time_set     !in: requested time in seconds
+        integer, intent(out):: time_handle !out: timer handle
         real(8):: val
+        timer_start=TIMERS_SUCCESS
         if(time_set.ge.0d0) then
-         if(handle_sp.ge.0.and.handle_sp.lt.max_timers) then
+!$OMP CRITICAL (TIMERS_REGION)
+         if(handle_sp.ge.0.and.handle_sp.lt.MAX_TIMERS) then
           time_handle=handle_stack(handle_sp); handle_sp=handle_sp+1
+         else
+          timer_start=TIMERS_ERR_NO_TIMERS_LEFT
+         endif
+!$OMP END CRITICAL (TIMERS_REGION)
+         if(timer_start.eq.TIMERS_SUCCESS) then
 #ifndef NO_OMP
           val=omp_get_wtime()
 #else
           call cpu_time(val)
 #endif
           timer(time_handle)=timer_t(val,time_set)
-          timer_start=0
-         else
-          timer_start=timers_err_no_timers_left
          endif
         else
-         timer_start=timers_err_invalid_arg
+         timer_start=TIMERS_ERR_INVALID_ARG
         endif
         return
         end function timer_start
 !-------------------------------------------------------------
         logical function time_is_off(time_handle,ierr,destroy)
-!This function tests whether a given timer has expired.
-!If <destroy> is present and .true., timer handle will be destroyed if the timer has expired.
+!This function tests whether a given timer had expired.
+!If <destroy> is present and .true., timer handle will be destroyed if the timer is expired.
         implicit none
-        integer, intent(inout):: time_handle !timer handle
-        integer, intent(inout):: ierr
-        logical, intent(in), optional:: destroy
+        integer, intent(inout):: time_handle    !inout: timer handle
+        integer, intent(inout):: ierr           !out: error code (0:success)
+        logical, intent(in), optional:: destroy !in: request to destroy the timer if expired
         real(8):: tm
         time_is_off=.false.
-        if(time_handle.ge.0.and.time_handle.lt.max_timers) then !valid range
+        if(time_handle.ge.0.and.time_handle.lt.MAX_TIMERS) then !valid range
          if(timer(time_handle)%time_interval.ge.0d0) then !valid handle
           ierr=0
 #ifndef NO_OMP
@@ -94,33 +99,37 @@
           if(tm.ge.timer(time_handle)%beg_time+timer(time_handle)%time_interval) time_is_off=.true.
           if(time_is_off.and.present(destroy)) then
            if(destroy) then
+!$OMP CRITICAL (TIMERS_REGION)
             timer(time_handle)=timer_t(-1d0,-1d0)
             handle_sp=handle_sp-1; handle_stack(handle_sp)=time_handle
+!$OMP END CRITICAL (TIMERS_REGION)
            endif
           endif
          else
-          ierr=timers_err_timer_null
+          ierr=TIMERS_ERR_TIMER_NULL
          endif
         else
-         ierr=timers_err_invalid_arg
+         ierr=TIMERS_ERR_INVALID_ARG
         endif
         return
         end function time_is_off
 !--------------------------------------------------
         integer function timer_destroy(time_handle)
-!This function frees a time handle.
+!This function explicitly frees a time handle.
         implicit none
         integer, intent(in):: time_handle
-        timer_destroy=0
-        if(time_handle.ge.0.and.time_handle.lt.max_timers) then !valid range
+        timer_destroy=TIMERS_SUCCESS
+        if(time_handle.ge.0.and.time_handle.lt.MAX_TIMERS) then !valid range
          if(timer(time_handle)%time_interval.ge.0d0) then !valid handle
+!$OMP CRITICAL (TIMERS_REGION)
           timer(time_handle)=timer_t(-1d0,-1d0)
           handle_sp=handle_sp-1; handle_stack(handle_sp)=time_handle
+!$OMP END CRITICAL (TIMERS_REGION)
          else
-          timer_destroy=timers_err_timer_null
+          timer_destroy=TIMERS_ERR_TIMER_NULL
          endif
         else
-         timer_destroy=timers_err_invalid_arg
+         timer_destroy=TIMERS_ERR_INVALID_ARG
         endif
         return
         end function timer_destroy
@@ -129,7 +138,9 @@
 !This function returns the wall clock tick length in seconds.
         implicit none
 #ifndef NO_OMP
+!$OMP CRITICAL (TIMERS_REGION)
         if(timer_tick.le.0d0) timer_tick=omp_get_wtick()
+!$OMP END CRITICAL (TIMERS_REGION)
 #endif
         timer_tick_sec=timer_tick
         return
@@ -142,7 +153,7 @@
 !This function returns the current wall clock time in seconds.
         implicit none
         real(8), intent(in), optional:: tbase
-        real(8) tm
+        real(8):: tm
 #ifndef NO_OMP
         thread_wtime=omp_get_wtime()
 #else
