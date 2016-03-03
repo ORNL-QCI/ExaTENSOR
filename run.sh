a40a525b6bcdec0c5726e0bc1c09911ca89998c4
Dmitry I. Lyakh <quant4me@gmail.com>
2016-03-03 15:09:49 -0500

    Updates the run.sh script.

--- run.sh
+++ run.sh
@@ -1,21 +1,21 @@
 #It is crucial to launch MPI processes consecutively within a node!
 #Environment variable QF_PROCS_PER_NODE must be set appropriately!
-export QFORCE_PATH=/ccs/home/div/src/ExaTensor      #mandatory
-#export QFORCE_PATH=/home/dima/Projects/QFORCE       #mandatory
-export QF_NUM_PROCS=1                               #mandatory
-export QF_PROCS_PER_NODE=1                          #mandatory
-export QF_CORES_PER_PROC=16                         #mandatory
-export QF_GPUS_PER_PROCESS=1                        #optional (Nvidia GPU)
-export QF_MICS_PER_PROCESS=0                        #optional (Intel Xeon Phi)
-export QF_AMDS_PER_PROCESS=0                        #optional (AMD GPU)
-export OMP_NUM_THREADS=8                            #mandatory
-export MKL_NUM_THREADS=$OMP_NUM_THREADS             #optional (CPU only: Intel MKL)
-export OMP_MAX_ACTIVE_LEVELS=3                      #mandatory
-export OMP_THREAD_LIMIT=64                          #optional
-export OMP_DYNAMIC=FALSE                            #mandatory
-export OMP_NESTED=TRUE                              #mandatory
-export OMP_WAIT_POLICY=PASSIVE                      #optional (idle thread behavior)
-#export KMP_AFFINITY=compact                        #optional (Intel CPU only)
+export QF_PATH=/ccs/home/div/src/ExaTensor          #full path to ExaTensor
+#export QF_PATH=/home/dima/Projects/QFORCE
+export QF_NUM_PROCS=1                               #number of MPI processes
+export QF_CORES_PER_PROC=16                         #number of cores per MPI process
+export QF_PROCS_PER_NODE=1                          #number of MPI processes per node
+export OMP_NUM_THREADS=8                            #max number of OpenMP threads per process
+export QF_GPUS_PER_PROCESS=1                        #number of Nvidia GPU's per process (optional)
+export QF_MICS_PER_PROCESS=0                        #number of Intel Xeon Phi's per process (optional)
+export QF_AMDS_PER_PROCESS=0                        #number of AMD GPU's per process (optional)
+export MKL_NUM_THREADS=$OMP_NUM_THREADS             #number of Intel MKL threads per process (optional)
+export OMP_MAX_ACTIVE_LEVELS=3                      #max number of OpenMP nesting levels (at least 3)
+export OMP_THREAD_LIMIT=64                          #max total number of OpenMP threads per process
+export OMP_DYNAMIC=FALSE                            #no OpenMP dynamic threading
+export OMP_NESTED=TRUE                              #OpenMP nested parallelism is mandatory
+export OMP_WAIT_POLICY=PASSIVE                      #idle thread behavior (optional)
+#export KMP_AFFINITY=compact                        #Intel CPU thread affinity (optional)
 export MIC_PREFIX=MIC                               #mandatory when using MIC
 export MIC_ENV_PREFIX=MIC                           #mandatory when using MIC
 export MIC_OMP_PREFIX=MIC                           #mandatory when using MIC
@@ -26,10 +26,17 @@ export MIC_KMP_AFFINITY="granularity=fine,compact"  #optional (MIC only)
 export MIC_USE_2MB_BUFFERS=64K                      #optional (MIC only)
 export MKL_MIC_ENABLE=0                             #optional (MIC only: MKL MIC auto-offloading)
 export OFFLOAD_REPORT=2                             #optional (MIC only)
-export CRAY_OMP_CHECK_AFFINITY=TRUE                 #optional (CRAY only: shows thread placement)
+
+#CRAY specific:
+export CRAY_OMP_CHECK_AFFINITY=TRUE                 #CRAY: shows thread placement (optional)
+export MPICH_NEMESIS_ASYNC_PROGRESS="SC"            #CRAY: Activates MPI asynchronous progress thread
+export MPICH_MAX_THREAD_SAFETY=multiple             #CRAY: Required for MPI asynchronous progress
+export MPICH_GNI_ASYNC_PROGRESS_TIMEOUT=0           #CRAY:
+export MPICH_RMA_OVER_DMAPP=1                       #CRAY: DMAPP backend for CRAY-MPICH
+export _DMAPPI_NDREG_ENTRIES=16384                  #CRAY: Max number of entries in UDREG memory registration cache
 
 rm *.tmp *.log *.out *.x
-cp $QFORCE_PATH/qforce.v13.01.x ./
+cp $QF_PATH/qforce.v13.01.x ./
 aprun -n $QF_NUM_PROCS -N $QF_PROCS_PER_NODE -d $QF_CORES_PER_PROC -cc 0,2,4,6,8,10,12,14,1,3,5,7,9,11,13,15 ./qforce.v13.01.x #> qforce.log
 #nvprof --log-file nv_profile.log --print-gpu-trace ./qforce.v13.01.x # &> qforce.log &
 #nvprof --log-file nv_profile.log --print-gpu-trace --metrics branch_efficiency,gld_efficiency,gst_efficiency ./qforce.v13.01.x # &> qforce.log &
