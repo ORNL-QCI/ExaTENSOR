d747b202f86b85d5c210fc73807c107f324c31c9
Dmitry Lyakh <quant4me@gmail.com>
2017-02-06 18:14:38 -0500

    Started gfc::graph.

--- GFC/gfc_graph.F90
+++ GFC/gfc_graph.F90
@@ -31,6 +31,9 @@
         integer(INTD), private:: CONS_OUT=6 !output device
         logical, private:: VERBOSE=.TRUE.   !verbosity for errors
         integer(INTD), private:: DEBUG=0    !debugging level (0:none)
+! Edge direction:
+        integer(INTD), parameter, public:: GFC_GRAPH_DIR_OUTWARD=-1
+        integer(INTD), parameter, public:: GFC_GRAPH_DIR_INWARD=+1
 !TYPES:
 #if 0
  !Graph edge:
@@ -39,6 +42,7 @@
          contains
           procedure, private:: GraphEdgeConstruct
           generic, public:: graph_edge_ctor=>GraphEdgeConstruct
+          procedure, public:: get_conn_vertex=>GraphEdgeGetConnVertex
         end type graph_edge_t
  !Directed colored graph edge (general graph edge):
         type, extends(graph_edge_t), public:: graph_edge_gen_t
@@ -46,6 +50,8 @@
          contains
           procedure, private:: GraphEdgeGenConstruct
           generic, public:: graph_edge_gen_ctor=>GraphEdgeGenConstruct
+          procedure, public:: get_color=>GraphEdgeGetColor
+          procedure, public:: get_direction=>GraphEdgeGetDirection
         end type graph_edge_gen_t
  !Graph vertex:
         type, public:: graph_vertex_t
@@ -62,12 +68,33 @@
          type(vector_t), private:: vertices
          integer(INTL), private:: num_edges=0_INTL
          contains
-          procedure, private:: is_empty=>GraphIsEmpty
-          procedure, private:: num_vertices=>GraphNumVertices
+          procedure, public:: is_empty=>GraphIsEmpty
+          procedure, public:: num_vertices=>GraphNumVertices
           final:: graph_dtor
         end type graph_t
  !Graph iterator:
         type, extends(gfc_iter_t), public:: graph_iter_t
+         class(graph_vertex_t), pointer, private:: current=>NULL()
+         class(graph_t), pointer, private:: container=>NULL()
+         contains
+          procedure, public:: init=>GraphIterInit
+          procedure, public:: reset=>GraphIterReset
+          procedure, public:: release=>GraphIterRelease
+          procedure, public:: pointee=>GraphIterPointee
+          procedure, public:: next=>GraphIterNext
+          procedure, public:: previous=>GraphIterPrevious
+          procedure, public:: get_num_vertices=>GraphIterGetNumVertices
+          procedure, public:: get_num_edges=>GraphIterGetNumEdges
+          procedure, public:: move_to=>GraphIterMoveTo
+          procedure, public:: get_edges=>GraphIterGetEdges
+          procedure, public:: append_vertex=>GraphIterAppendVertex
+          procedure, public:: append_edge=>GraphIterAppendEdge
+          procedure, public:: merge_vertex=>GraphIterMergeVertex
+          procedure, public:: split_vertex=>GraphIterSplitVertex
+          procedure, public:: delete_vertex=>GraphIterDeleteVertex
+          procedure, public:: delete_edge=>GraphIterDeleteEdge
+          procedure, public:: delete_all=>GraphIterDeleteAll
+          final:: graph_iter_dtor
         end type graph_iter_t
 #endif
        end module gfc_graph
