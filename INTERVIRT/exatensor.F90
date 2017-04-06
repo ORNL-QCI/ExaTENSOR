ab8e51d44bca440f4c75118cb1b86baacb74383e
Dmitry I. Lyakh <quant4me@gmail.com>
2017-04-06 18:31:43 -0400

    Specified the core ExaTENSOR API.

--- INTERVIRT/exatensor.F90
+++ INTERVIRT/exatensor.F90
@@ -1,7 +1,7 @@
 !ExaTENSOR: Massively Parallel Virtual Processor for Scale-Adaptive Tensor Algebra
-!This is the top level API module of ExaTENSOR
+!This is the top level API module of ExaTENSOR (user space API)
 !AUTHOR: Dmitry I. Lyakh (Liakh): quant4me@gmail.com
-!REVISION: 2017/03/20
+!REVISION: 2017/04/06
 
 !Copyright (C) 2014-2017 Dmitry I. Lyakh (Liakh)
 !Copyright (C) 2014-2017 Oak Ridge National Laboratory (UT-Battelle)
@@ -40,18 +40,43 @@
 !DATA:
 
 !VISIBILITY:
- !Control API:
-       public exa_tensor                    !entry point into ExaTensor (debug)
+       public exa_tensor                  !entry point into ExaTensor (debug)
 #if 0
-       public exatns_start                  !starts the ExaTENSOR DSVP
-       public exatns_stop                   !stops the ExaTENSOR DSVP
+ !Control:
+       public exatns_start                !starts the ExaTENSOR DSVP
+       public exatns_stop                 !stops the ExaTENSOR DSVP
+       public exatns_status               !returns the status of the ExaTENSOR DSVP (plus statistics, if needed)
+ !Symbols:
+       public exatns_symbol_exists        !checks whether a specific identifier is registered (if yes, returns its attributes)
+ !External methods/data:
+       public exatns_method_register      !registers an external method (it has to adhere to a predefined interface)
+       public exatns_method_unregister    !unregisters an external method
+       public exatns_data_register        !registers external data (for future references)
+       public exatns_data_unregister      !unregisters external data
  !Hierarchical vector space:
-       public exatns_space_register         !registers a vector space
-       public exatns_space_unregister       !unregisters a vector space
-       public exatns_subspace_register      !registers a subspace in a vector space
-       public exatns_subspace_unregister    !unregisters a subspace in a vector space
-       public exatns_index_register         !assigns an index label to a specific space/subspace
-       public exatns_index_unregister       !unregisters an index label
+       public exatns_space_register       !registers a vector space
+       public exatns_space_unregister     !unregisters a vector space
+       public exatns_space_build_tree     !builds the subspace aggregation tree (SAT) in a vector space
+       public exatns_space_status         !returns the status of the vector space
+       public exatns_subspace_register    !registers a subspace in a vector space
+       public exatns_subspace_unregister  !unregisters a subspace in a vector space
+       public exatns_index_register       !associates an index label with a specific space/subspace
+       public exatns_index_unregister     !unregisters an index label
+ !Tensor:
+       public exatns_tensor_create        !creates an empty tensor with an optional deferred initialization method
+       public exatns_tensor_destroy       !destroys a tensor
+       public exatns_tensor_load          !loads a tensor from persistent storage (create + populate)
+       public exatns_tensor_save          !saves a tensor to persistent storage
+       public exatns_tensor_status        !returns the status of the tensor (e.g., empty, initialized, being updated, etc.)
+ !Tensor operations:
+       public exatns_tensor_init          !initializes the tensor to a real/complex value or invokes an initialization method
+       public exatns_tensor_copy          !copies a tensor into another tensor, possibly with permutation, slicing, or insertion
+       public exatns_tensor_fold          !produces a new tensor by folding multiple tensor dimensions into a single one
+       public exatns_tensor_unfold        !produces a new tensor by unfolding a tensor dimension into multiple dimensions
+       public exatns_tensor_scale         !multiplies all tensor elements by a real/complex number
+       public exatns_tensor_add           !adds a tensor to another tensor, possibly with permutation, slicing, or insertion
+       public exatns_tensor_contract      !contracts two tensors to produce another tensor (tensor product is included as a special case)
+       public exatns_tensor_operation     !custom tensor operation via user-defined (external) methods
 #endif
       contains
 !IMPLEMENTATION:
