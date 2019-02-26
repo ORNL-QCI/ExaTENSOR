3d7dfbabbbe1ab36f8d5ec19e0507910212e7489
Dmitry I. Lyakh <quant4me@gmail.com>
2019-02-26 12:09:54 -0500

    Fixed BytePacket template methods.

--- TALSH/byte_packet.cpp
+++ TALSH/byte_packet.cpp
@@ -6,12 +6,7 @@ Copyright (C) 2018-2019 Oak Ridge National Laboratory (UT-Battelle) **/
 
 #include "byte_packet.h"
 
-#include <assert.h>
 #include <cstdlib>
-
-#ifdef __cplusplus
-#include <cstddef>
-#endif
 
 void initBytePacket(BytePacket * packet)
 {
@@ -38,30 +33,3 @@ void resetBytePacket(BytePacket * packet)
  packet->position = 0;
  return;
 }
-
-#ifdef __cplusplus
-template <typename T>
-void appendToBytePacket(BytePacket * packet, const T & item)
-{
- char * dst_ptr = &(((char*)(packet->base_addr))[packet->position]);
- char * src_ptr = ((char*)(&item));
- unsigned long long type_size = sizeof(T);
- assert(packet->position + type_size <= packet->capacity);
- for(unsigned long long i = 0; i < type_size; ++i) dst_ptr[i] = src_ptr[i];
- packet->position += type_size;
- if(packet->position > packet->size_bytes) packet->size_bytes = packet->position;
- return;
-}
-
-template <typename T>
-void extractFromBytePacket(BytePacket * packet, T & item)
-{
- char * src_ptr = &(((char*)(packet->base_addr))[packet->position]);
- char * dst_ptr = ((char*)(&item));
- unsigned long long type_size = sizeof(T);
- assert(packet->position + type_size <= packet->size_bytes);
- for(unsigned long long i = 0; i < type_size; ++i) dst_ptr[i] = src_ptr[i];
- packet->position += type_size;
- return;
-}
-#endif
--- TALSH/byte_packet.h
+++ TALSH/byte_packet.h
@@ -7,6 +7,12 @@ Copyright (C) 2018-2019 Oak Ridge National Laboratory (UT-Battelle) **/
 #ifndef BYTE_PACKET_H_
 #define BYTE_PACKET_H_
 
+#include <assert.h>
+
+#ifdef __cplusplus
+#include <cstddef>
+#endif
+
 #define BYTE_PACKET_CAPACITY 1048576 //default byte packet capacity in bytes
 
 //Byte packet (interoperable):
@@ -21,9 +27,32 @@ typedef struct{
 void initBytePacket(BytePacket * packet);
 void clearBytePacket(BytePacket * packet);
 void resetBytePacket(BytePacket * packet);
+
 #ifdef __cplusplus
-template <typename T> void appendToBytePacket(BytePacket * packet, const T & item);
-template <typename T> void extractFromBytePacket(BytePacket * packet, T & item);
+template <typename T>
+void appendToBytePacket(BytePacket * packet, const T & item)
+{
+ char * dst_ptr = &(((char*)(packet->base_addr))[packet->position]);
+ char * src_ptr = ((char*)(&item));
+ unsigned long long type_size = sizeof(T);
+ assert(packet->position + type_size <= packet->capacity);
+ for(unsigned long long i = 0; i < type_size; ++i) dst_ptr[i] = src_ptr[i];
+ packet->position += type_size;
+ if(packet->position > packet->size_bytes) packet->size_bytes = packet->position;
+ return;
+}
+
+template <typename T>
+void extractFromBytePacket(BytePacket * packet, T & item)
+{
+ char * src_ptr = &(((char*)(packet->base_addr))[packet->position]);
+ char * dst_ptr = ((char*)(&item));
+ unsigned long long type_size = sizeof(T);
+ assert(packet->position + type_size <= packet->size_bytes);
+ for(unsigned long long i = 0; i < type_size; ++i) dst_ptr[i] = src_ptr[i];
+ packet->position += type_size;
+ return;
+}
 #endif
 
 #endif //BYTE_PACKET_H_
